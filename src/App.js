2e9c38dba885ed0e5608497f2d0ea1c88e8133f4
Bmattholder <bmattholder@gmail.com>
2023-03-08 20:55:40 -0600

    cruddy crud app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import ItemList from './components/ItemList';
+import ItemDetail from './components/ItemDetail';
+import ItemForm from './components/ItemForm';
+import { useState } from 'react';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  const addItem = (item) => {
+    setItems([...items, item]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Container className='mt-3'>
+        <Routes>
+          <Route
+            path='/'
+            element={<ItemList items={items} addItem={addItem} />}
+          />
+          <Route path='/items/new' element={<ItemForm addItem={addItem} />} />
+          <Route path='/items/:id' element={<ItemDetail items={items} />} />
+          <Route
+            path='/items/:id/edit'
+            element={<ItemForm items={items} addItem={addItem} />}
+          />
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 
