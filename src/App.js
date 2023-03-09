c9f2673b56d5aef1a86d331cf0d7fb1828844319
Bmattholder <bmattholder@gmail.com>
2023-03-09 15:35:50 -0600

    Mar-9

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import InputForm from './components/InputForm';
+import PersonList from './components/PersonList';
 
 function App() {
+  const [people, setPeople] = useState([]);
+  const [editPerson, setEditPerson] = useState(null);
+
+  const peopleHandler = (person) => {
+    if (editPerson) {
+      const editedPeople = people.map(p => p === editPerson ? person : p);
+      setPeople(editedPeople);
+      setEditPerson(null);
+    } else {
+      setPeople((prevState) => [...prevState, person]);
+    }
+  };
+
+  const handleDelete = (index) => {
+    const updatedPeople = people.filter((_, i) => i !== index);
+    setPeople(updatedPeople);
+  };
+
+  const handleEdit = (index) => {
+    const person = people[index];
+    setEditPerson(person);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <InputForm peopleHandler={peopleHandler} editPerson={editPerson} />
+      <br />
+      <PersonList people={people} handleDelete={handleDelete} handleEdit={handleEdit} />
+    </>
   );
 }
 
