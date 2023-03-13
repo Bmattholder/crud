efc3274988516968bcfd1467dd7089c9e7ac7455
Bmattholder <bmattholder@gmail.com>
2023-03-13 18:19:05 -0500

    Mar13

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Form from './components/Form';
+import List from './components/List';
 
 function App() {
+  const [peopleArray, setPeopleArray] = useState([
+    {
+      id: 1,
+      name: 'johnny',
+    },
+    {
+      id: 2,
+      name: 'sally',
+    },
+  ]);
+
+  const addPerson = (formData) => {
+    setPeopleArray((prevState) => [
+      ...prevState,
+      {
+        id: formData.id,
+        name: formData.name,
+      },
+    ]);
+  };
+
+  const updatePerson = (id, name) => {
+    setPeopleArray((prevState) =>
+      prevState.map((person) =>
+        person.id === id ? { ...person, name: name } : person
+      )
+    );
+  };
+
+  const deletePerson = (id) => {
+    setPeopleArray((prevState) => prevState.filter((person) => person.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Form functionToPass={addPerson} />
+      <br />
+      <List peopleArray={peopleArray} updatePerson={updatePerson} deletePerson={deletePerson} />
+    </>
   );
 }
 
